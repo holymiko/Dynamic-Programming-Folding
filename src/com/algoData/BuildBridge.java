6616ecfa2d90e6637d2755385e7ce9e29a80e60e
holymiko <holymiko@fit.cvut.cz>
2021-12-12 02:38:27 +0100

    foldMethod optimization (tested)

--- src/com/algoData/BuildBridge.java
+++ src/com/algoData/BuildBridge.java
@@ -10,12 +10,11 @@ public class BuildBridge {
 
 
     private int foldCount;
-    private List<Fold> foldVector;
+    private final List<Fold> foldVector;
 
     public BuildBridge() {
         foldCount = 0;
         foldVector = new Vector<>();
-
     }
 
     private void initFoldVector(String input) {
@@ -29,7 +28,16 @@ public class BuildBridge {
     }
 
 
-    private boolean isFoldPossible(int leftIndex, int rightIndex) {
+    /**
+     * Matches opposite FOLD values in foldVector.
+     * Indexes of matching are moving towards each other.
+     * If the indexes meet, all Folds fit and sides get be folded on each other.
+     * @param leftIndex .
+     * @param rightIndex .
+     * @throws IllegalArgumentException - When sum of arguments is ODD
+     * @return True - Fold with given positions can be performed
+     */
+    private boolean isFoldPossible(int leftIndex, int rightIndex) throws IllegalArgumentException {
 //        System.out.println("L "+leftIndex+"  R "+rightIndex);
         if(leftIndex+rightIndex % 2 == 1) {
             throw new IllegalArgumentException("Range has to have ODD SIZE");
@@ -51,45 +59,48 @@ public class BuildBridge {
         return true;
     }
 
+    private void performFold(int foldSize) {
+        // TODO Perform fold
+//                foldVector = foldVector.subList(0, foldVector.size() - 1 - foldSize*2);
+
+        // TODO Is subList more efficient?
+        // Deleting foldSize + 1 // +1 because of the place where fold will be performed
+        for (int i = 0; i < foldSize; i++) {
+            foldVector.remove( foldVector.size() - 1 );
+        }
+    }
+
     /**
      * Performs 1 fold.
      * Tries to perform biggest currently possible fold.
      * Goes from MAX size of fold to size of 1
      */
     private void foldMethod() {
+        final int afterLastIndex = foldVector.size();
+
         // Init index
         int middleIndex = getMiddleIndex();
-        int afterLastIndex = foldVector.size();
+        // Number of edges (folds) which will reduce foldVector
+        int foldSize = afterLastIndex - middleIndex;
 
+        while( 0 < foldSize ) {
 
-        while(middleIndex < afterLastIndex) {
-            // Number of edges (folds) which will reduce foldVector
-            final int foldSize = afterLastIndex - middleIndex;
             final int leftIndex = middleIndex - foldSize + 1;
             final int rightIndex = foldVector.size() - 1;
-            boolean isPossible = isFoldPossible(leftIndex, rightIndex);
 
-            if(isPossible) {
+            if( isFoldPossible(leftIndex, rightIndex) ) {
                 System.out.println("L "+leftIndex+"  R "+rightIndex);
-
-//                System.out.println( "  "+ (isPossible ? "True" : "False" ) ) ;
                 System.out.print("Fold size "+foldSize);
                 System.out.println( "  Index: "+middleIndex );
                 System.out.println();
 
-                // TODO Perform fold
-//                foldVector = foldVector.subList(0, foldVector.size() - 1 - foldSize*2);
-
-                // TODO Is subList more efficient?
-                // Deleting foldSize + 1 // +1 because of the place where fold will be performed
-                for (int i = 0; i < foldSize; i++) {
-                    foldVector.remove( foldVector.size() - 1 );
-                }
+                performFold(foldSize);
                 break;
             }
 
             // Move index to the right
             middleIndex++;
+            foldSize--;
         }
     }
 
@@ -104,6 +115,11 @@ public class BuildBridge {
         return foldVector.size() / 2;
     }
 
+    private int findMaxFold(int leftIndex, int rightIndex) {
+        int max = 0;
+        return max;
+    }
+
     public void run() {
         // Load data
         String stringStrip = readInput();
@@ -112,7 +128,9 @@ public class BuildBridge {
         printVector();
 
 
+
         while (foldVector.size() > 0) {
+            findMaxFold(0, foldVector.size()-1);
             foldMethod();
             foldCount++;
 //            break;
